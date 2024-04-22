e69cf033bb03cd3f87d065dca44254fb46f673d3
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2024-04-22 21:25:14 +0530

    get date a calender form

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,26 @@
 import './ExpenseItem.css'
 
+const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
 function ExpenseItem(props) {
-  const expenseDate = props.date.toISOString();
-  const expenseAmount=props.price;
-  const location=props.location;
-  const title=props.title;
+  const expenseDate = props.date.getDate();
+  const expenseMonth=months[props.date.getMonth()];
+  const expenseYear=props.date.getFullYear();
+  
   return (
     <div className='expense-item'>
-      <div>{expenseDate}</div>
-      <div className="expense-item__location">{location}</div>
+      <div>
+        <div>{expenseMonth}</div>
+        <div>{expenseYear}</div>
+        <div>{expenseDate}</div>
+      </div>
+      <div className="expense-item__location">{props.location}</div>
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.price}</div>
       </div>
     </div>
   );
